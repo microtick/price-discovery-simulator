a605030c4d6aeb0e2c714ec909ced3ffa3e5c519
Mark <mjackson@microtick.com>
2020-03-25 13:55:53 -0600

    remove unused controls

--- src/containers/Controls.js
+++ src/containers/Controls.js
@@ -7,18 +7,18 @@ class Controls extends React.Component {
     const timespans = this.props.timespans.map((t, n) => {
       return <button key={n}>{t}</button>
     })
+      //<span id="datatype">
+        //<button>Tick</button>
+        //<button>1-minute Bars</button>
+        //<button>5-minute Bars</button>
+      //</span>
+      //<span id="timespans">
+        //{timespans}
+      //</span>
     return <div id="controls">
       <span id="startstop">
         <button onClick={this.props.start}>Start</button>
         <button onClick={() => this.props.stop()}>Stop</button>
-      </span>
-      <span id="datatype">
-        <button>Tick</button>
-        <button>1-minute Bars</button>
-        <button>5-minute Bars</button>
-      </span>
-      <span id="timespans">
-        {timespans}
       </span>
     </div>
   }
